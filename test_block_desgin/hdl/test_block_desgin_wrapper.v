d822dbd135fc3d86451b5fb182bd97232bfe9af6
Takeru Hayasaka <hayatake396@gmail.com>
2025-03-24 04:44:35 +0900

    chore: add artifact

---
+++ test_block_desgin/hdl/test_block_desgin_wrapper.v
@@ -0,0 +1,19 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2024.1 (lin64) Build 5076996 Wed May 22 18:36:09 MDT 2024
+//Date        : Sun Mar 23 18:23:45 2025
+//Host        : 08208fd6c71b running 64-bit Ubuntu 22.04.5 LTS
+//Command     : generate_target test_block_desgin_wrapper.bd
+//Design      : test_block_desgin_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module test_block_desgin_wrapper
+   ();
+
+
+  test_block_desgin test_block_desgin_i
+       ();
+endmodule
---
+++ test_block_desgin/synth/test_block_desgin.v
@@ -0,0 +1,18 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2024.1 (lin64) Build 5076996 Wed May 22 18:36:09 MDT 2024
+//Date        : Sun Mar 23 18:23:45 2025
+//Host        : 08208fd6c71b running 64-bit Ubuntu 22.04.5 LTS
+//Command     : generate_target test_block_desgin.bd
+//Design      : test_block_desgin
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+(* CORE_GENERATION_INFO = "test_block_desgin,IP_Integrator,{x_ipVendor=xilinx.com,x_ipLibrary=BlockDiagram,x_ipName=test_block_desgin,x_ipVersion=1.00.a,x_ipLanguage=VERILOG,numBlks=0,numReposBlks=0,numNonXlnxBlks=0,numHierBlks=0,maxHierDepth=0,numSysgenBlks=0,numHlsBlks=0,numHdlrefBlks=0,numPkgbdBlks=0,bdsource=USER,synth_mode=Hierarchical}" *) (* HW_HANDOFF = "test_block_desgin.hwdef" *) 
+module test_block_desgin
+   ();
+
+
+endmodule
